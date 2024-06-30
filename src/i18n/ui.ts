c945350cec61b9b8f8f7cb60ff7c97fa4e316ba8
Iv44n <ivandev2oo6@gmail.com>
2024-06-30 15:33:47 -0500

    Added UI configuration for i18n support

---
+++ src/i18n/ui.ts
@@ -0,0 +1,30 @@
+export const languages = {
+    es: "Español",
+    en: "English"
+}
+
+export const defaultLang = "es"
+export const showDefaultLang = false;
+
+export const ui = {
+    es:{
+        'nav.projects': 'proyectos',
+        'nav.about': 'sobre-mí',
+        'nav.contact': 'contacto',
+        'nav.cv': 'Descargar-cv',
+        'nav-buttons.auto': 'Automático',
+        'nav-buttons.light': 'Claro',
+        'nav-buttons.dark': 'Oscuro',
+        'footer.made': 'Realizado por Ivan. Código fuente del sitio web en',
+    },
+    en:{
+        'nav.projects': 'projects',
+        'nav.about': 'about-me',
+        'nav.contact': 'contact',
+        'nav.cv': 'Download-cv',
+        'nav-buttons.auto': 'Auto',
+        'nav-buttons.light': 'Light',
+        'nav-buttons.dark': 'Dark',
+        'footer.made': 'Made by Ivan. Website source code in',
+    }
+} as const
