869f81f6d8cdf022567a84fb9f307cc0300401ec
Iv44n <ivandev2oo6@gmail.com>
2024-07-05 16:00:38 -0500

    Added internationalization functionality with support for English and Spanish using JSON files

---
+++ src/i18n/index.ts
@@ -0,0 +1,14 @@
+import english from '@i18n/translations/en.json';
+import spanish from '@i18n/translations/es.json';
+
+const LANGUAGES = {
+    ENGLISH: 'en',
+    SPANISH: 'es',
+};
+
+export const getI18n = ({ currentLocale = 'es' }: { currentLocale?: string }) => {
+    if (currentLocale === LANGUAGES.ENGLISH) {
+        return { ...spanish, ...english };
+    }
+    return spanish;
+};
