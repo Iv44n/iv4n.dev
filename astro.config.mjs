f4a90a8fe62e3bb65b4143180292fecfba45f08d
Iv44n <ivandev2oo6@gmail.com>
2024-07-26 11:41:48 -0500

    Replace Netlify adapter with Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapter: netlify(),
+  adapter: vercel(),
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
