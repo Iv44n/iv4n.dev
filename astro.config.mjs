b91a770dbf874606f085de571bc79818cbeff8df
Iv44n <ivandev2oo6@gmail.com>
2024-08-14 22:55:52 -0500

    Added site URL to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://iv4n.dev',
   output: 'hybrid',
   adapter: vercel(),
   i18n: {
