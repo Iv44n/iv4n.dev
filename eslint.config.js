6c2ca87e3bb3a7abd7c7f8b6c849f227f77a5540
Iv44n <ivandev2oo6@gmail.com>
2024-11-05 10:49:50 -0500

    chore: setup ESLint with Astro plugin and fix linting errors

---
+++ eslint.config.js
@@ -0,0 +1,60 @@
+import globals from 'globals'
+import jsPlugins from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintPluginAstro from 'eslint-plugin-astro'
+
+const config = [
+  // Configuración general para todos los archivos JS, TS y Astro
+  {
+    files: ['**/*.{js,mjs,cjs,ts,astro}'],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node }
+    },
+    rules: {
+      indent: ['error', 2],
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      'no-trailing-spaces': 'error',
+      'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+      'eol-last': 'error',
+      'comma-dangle': ['error', 'never'],
+      'block-spacing': ['error', 'always'],
+      'key-spacing': ['error', { beforeColon: false, afterColon: true }],
+      'no-duplicate-imports': 'error',
+      'comma-spacing': ['error', { before: false, after: true }],
+      'object-curly-spacing': ['error', 'always'],
+      camelcase: 'error',
+      'astro/no-unused-css-selector': 'off',
+      'astro/semi': ['error', 'never'],
+      'jsx-quotes': ['error', 'prefer-single']
+    }
+  },
+
+  // Configuración específica para archivos .js
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs'
+    }
+  },
+
+  // Incluir configuraciones recomendadas de ESLint para JS y TS
+  jsPlugins.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs['flat/recommended'],
+
+  // Configuración específica para archivos .d.ts (desactiva triple-slash reference)
+  {
+    files: ['**/*.d.ts'],
+    rules: {
+      '@typescript-eslint/triple-slash-reference': 0
+    }
+  },
+
+  // Carpetas ignoradas
+  {
+    ignores: ['dist/', 'node_modules/', '.vscode/', '.astro/']
+  }
+]
+
+export default config
