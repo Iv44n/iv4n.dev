accd0a0f445a3f3f496dcbce42ec3fdbbbd2c6d1
Iv44n <ivandev2oo6@gmail.com>
2024-11-09 10:08:34 -0500

    feat(eslint): add stylistic rules for enhanced formatting across JS, TS, and JSX

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,9 @@ import globals from 'globals'
 import jsPlugins from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import eslintPluginAstro from 'eslint-plugin-astro'
+import stylistic from '@stylistic/eslint-plugin'
+import stylisticTs from '@stylistic/eslint-plugin-ts'
+import stylisticJsx from '@stylistic/eslint-plugin-jsx'
 
 const config = [
   // Configuración general para todos los archivos JS, TS y Astro
@@ -10,23 +13,42 @@ const config = [
     languageOptions: {
       globals: { ...globals.browser, ...globals.node }
     },
+    plugins: {
+      '@stylistic': stylistic,
+      '@stylistic/ts': stylisticTs,
+      '@stylistic/jsx': stylisticJsx
+    },
     rules: {
-      indent: ['error', 2],
-      quotes: ['error', 'single'],
-      semi: ['error', 'never'],
-      'no-trailing-spaces': 'error',
-      'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
-      'eol-last': 'error',
-      'comma-dangle': ['error', 'never'],
-      'block-spacing': ['error', 'always'],
-      'key-spacing': ['error', { beforeColon: false, afterColon: true }],
-      'no-duplicate-imports': 'error',
-      'comma-spacing': ['error', { before: false, after: true }],
-      'object-curly-spacing': ['error', 'always'],
-      camelcase: 'error',
+      '@stylistic/no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+      '@stylistic/no-trailing-spaces': 'error',
+      '@stylistic/eol-last': 'error',
+      '@stylistic/jsx-quotes': ['error', 'prefer-single'],
+      '@stylistic/ts/indent': ['error', 2],
+      '@stylistic/ts/quotes': ['error', 'single'],
+      '@stylistic/ts/semi': ['error', 'never'],
+      '@stylistic/ts/comma-dangle': ['error', 'never'],
+      '@stylistic/ts/block-spacing': ['error', 'always'],
+      '@stylistic/ts/key-spacing': ['error', { beforeColon: false, afterColon: true }],
+      '@stylistic/ts/comma-spacing': ['error', { before: false, after: true }],
+      '@stylistic/ts/object-curly-spacing': ['error', 'always'],
+      '@stylistic/ts/brace-style': 'error',
+      '@stylistic/ts/function-call-spacing': ['error', 'never'],
+      '@stylistic/ts/padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', prev: ['case', 'default'], next: '*' }
+      ],
+      '@stylistic/ts/space-infix-ops': 'error',
+      '@stylistic/ts/type-annotation-spacing': 'error',
+      '@stylistic/jsx/jsx-closing-bracket-location': 1,
+      '@stylistic/jsx/jsx-closing-tag-location': 1,
+      '@stylistic/jsx/jsx-curly-brace-presence': [
+        'error',
+        { props: 'never', children: 'never', propElementValues: 'always' }
+      ],
+      '@stylistic/jsx/jsx-curly-newline': 'error',
       'astro/no-unused-css-selector': 'off',
       'astro/semi': ['error', 'never'],
-      'jsx-quotes': ['error', 'prefer-single']
+      'no-duplicate-imports': 'error'
     }
   },
 
